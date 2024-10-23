0880699e9ac14f919ddf72e22b5a96da2ea5854d
Udara Wijesinghe <udarawijesinghe20@gmail.com>
2024-10-23 15:21:03 +0530

    registration service

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+
+// import the necessary modules
+const sequelize = require('./config/db');
+
 const app = express();
 
 const port = 3000;
@@ -7,6 +11,10 @@ app.get('/', (req, res) => {
     // run the coomand "npm run dev" start backend using the nodemon
   res.send('This is BallotGo Backend!');
 });
+
+sequelize.sync().then(() => {
+  console.log('Database connected and models synchronized');
+}).catch(err => console.error('Error connecting to the database:', err));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db'); // Adjust the path as necessary
+
+const User = sequelize.define('User', {
+  identity_card_number: {
+    type: DataTypes.STRING(20),
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  mobile_number: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  residence_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+  },
+  date_of_birth: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+}, {
+  tableName: 'user',
+  timestamps: true, // Automatically adds createdAt and updatedAt timestamps
+});
+
+module.exports = User;
