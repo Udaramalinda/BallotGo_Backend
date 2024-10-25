e256865985be406ba4ff693519ecec575883eebb
sanujaediri <sanujaedirisinghe@gmail.com>
2024-10-25 17:54:57 +0530

    cors added. Login fixed

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // import the necessary modules
-const sequelize = require('./config/db');
+const sequelize = require("./config/db");
 
 // import the routes
-const userRoutes = require('./routes/user.routes');
-const candidateRoutes = require('./routes/candidate.routes');
+const userRoutes = require("./routes/user.routes");
+const candidateRoutes = require("./routes/candidate.routes");
 
 // import the models
-const { User } = require('./models/user.model');
-const { UserPassword } = require('./models/userPassword.model');
-const { TempUser } = require('./models/tempUser.model');
+const { User } = require("./models/user.model");
+const { UserPassword } = require("./models/userPassword.model");
+const { TempUser } = require("./models/tempUser.model");
+const cors = require("cors");
 
 module.exports = {
   User,
@@ -24,25 +25,37 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    // run the coomand "npm run dev" start backend using the nodemon
-  res.send('This is BallotGo Backend!');
+// Use CORS middleware
+app.use(
+  cors({
+    origin: "*", // Allow only requests from this origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods if needed
+    credentials: true, // Include this if you need to send cookies or other credentials
+  })
+);
+
+app.get("/", (req, res) => {
+  // run the coomand "npm run dev" start backend using the nodemon
+  res.send("This is BallotGo Backend!");
 });
 
 // use the routes
-app.use('/api/user', userRoutes);
-app.use('/api/candidate', candidateRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/candidate", candidateRoutes);
 
 try {
   sequelize.authenticate();
-  console.log('Connection has been established successfully.');
+  console.log("Connection has been established successfully.");
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+  console.error("Unable to connect to the database:", error);
 }
 
-// sequelize.sync({ force: true }).then(() => {
-//   console.log('Database connected and models synchronized');
-// }).catch(err => console.error('Error connecting to the database:', err));
+// sequelize
+//   .sync({ force: true })
+//   .then(() => {
+//     console.log("Database connected and models synchronized");
+//   })
+//   .catch((err) => console.error("Error connecting to the database:", err));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
